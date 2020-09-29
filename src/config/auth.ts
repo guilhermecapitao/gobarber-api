02dfc474d6782c84fccb39a7bc2eeece2b5f03f9
guilhermecapitao <guilherme.capitao95@gmail.com>
2020-09-28 23:16:47 -0300

    feat: Authentication

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'f7958471b2cbfc3355ce904a91a40413',
+    expiresIn: '1d',
+  },
+};
---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,38 @@
+import { Request, Response, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
+
+import expiresIn from '../config/auth';
+
+interface TokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
+export default function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): void {
+  const authheader = request.headers.authorization;
+
+  if (!authheader) {
+    throw new Error('JWT token is missing');
+  }
+
+  const [, token] = authheader.split(' ');
+
+  try {
+    const decoded = verify(token, expiresIn.jwt.secret);
+
+    const { sub } = decoded as TokenPayload;
+
+    request.user = {
+      id: sub,
+    };
+
+    return next();
+  } catch {
+    throw new Error('Invalid JWT token');
+  }
+}
